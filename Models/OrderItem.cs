57230cef9128cc99a0ea46d084155d101f8f9127
Taylor Seager <taylorseager718@gmail.com>
2024-08-26 18:19:14 -0500

    added orderitem.cs file and class

---
+++ Models/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BackEnd.Models
+{
+	public class OrderItem
+	{
+        public int Id { get; set; }
+        public int ProductId { get; set; } // Foreign Key to Product
+        public int OrderId { get; set; } // Foreign Key to Order
+        public int Quantity { get; set; }
+    }
+}
+
